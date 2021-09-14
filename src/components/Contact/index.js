2903a2922072d52dfba44bc619065058b256bfc3
vinod <tvinodkumar354@gmail.com>
2021-09-14 09:51:02 +0000

    second commit

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,11 +2,7 @@ import './index.css'
 
 const Contact = () => (
   <div className="contact-container">
-    <img
-      src="https://assets.ccbp.in/frontend/react-js/contact-blog-img.png"
-      alt="contact"
-      className="contact-img"
-    />
+    <img src="https://cutt.ly/EW9rB9a" alt="contact" className="contact-img" />
     <h1 className="contact-heading">Contact</h1>
   </div>
 )
--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -3,7 +3,7 @@ import './index.css'
 const NotFound = () => (
   <div className="not-found-container">
     <img
-      src="https://assets.ccbp.in/frontend/react-js/not-found-blog-img.png"
+      src="https://cutt.ly/nW9tRjZ"
       alt="not-found"
       className="not-found-img"
     />

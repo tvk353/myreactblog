6f840c6d83d80eac65f4848b1d4c1cae2c95069e
vinod <tvinodkumar354@gmail.com>
2021-09-14 09:57:43 +0000

    first commit

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -2,7 +2,7 @@ import './index.css'
 
 const About = () => (
   <div className="about-container">
-    <img src="https://cutt.ly/UW9rE7j" alt="about" className="about-img" />
+    <img src="https://cutt.ly/1W9yhIg" alt="about" className="about-img" />
     <h1 className="about-heading">About</h1>
     <p className="about-paragraph">All about Blogs of developers</p>
   </div>
